9ec3e39163736dc5ee14aa2b4154b5baf3a22472
aadi-thedeyguy <adikhare24@gmail.com>
2022-12-23 12:00:28 +0530

    implemented layout of all pages with routing

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,73 +1,56 @@
-import React from 'react'
-import { Badge, Button, Figure, Table } from 'react-bootstrap'
+import { useContext } from 'react'
+import { Badge, Button, Figure, Stack } from 'react-bootstrap'
+import ListGroup from 'react-bootstrap/ListGroup';
+import Container from 'react-bootstrap/Container';
+import { CartContext } from '../context/CartContext'
 
-const cartElements = [
-
-    {
-        title: 'Colors',
-        price: 100,
-        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
-        quantity: 2,
-    },
-    {
-        title: 'Black and white Colors',
-        price: 50,
-        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
-        quantity: 3,
-    },
-    {
-        title: 'Yellow and Black Colors',
-        price: 70,
-        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
-        quantity: 1,
-    }
-]
 function Cart() {
+
+    const { cartItems } = useContext(CartContext)
+
     return (
-        <Table borderless striped size="sm">
-            <thead>
-                <tr>
-                    <th>Item</th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    cartElements.map(el => (
-                        <tr>
+        <div >
+            <Stack direction="horizontal" className='flex justify-content-between' gap="3">
+                <h2>Item</h2>
+                <h2>Price</h2>
+                <h2>Quantity</h2>
 
-                            <td>
-                                <Figure>
-                                    <Figure.Image
-                                        width={80}
-                                        height={60}
-                                        alt={el.title}
-                                        src={el.imageUrl}
-                                    />
-                                    <Figure.Caption>
-                                        {el.title}
-                                    </Figure.Caption>
-                                </Figure>
+            </Stack>
+            {
+                cartItems.map(el => (
+                    <Stack direction="horizontal" className='flex justify-content-between' key={el.id}>
 
-                            </td>
-                            <td>${el.price}</td>
-                            <td>
-                                <Badge pill bg="info">
-                                    {el.quantity}
-                                </Badge>{' '}
-                                <Button variant="danger">Remove</Button>
+                        <Figure className='d-flex flex-column'>
+                            <Figure.Image
+                                width={60}
+                                height={40}
+                                alt={el.title}
+                                src={el.imageUrl}
+                            />
+                            <Figure.Caption>
+                                {el.title}
+                            </Figure.Caption>
+                        </Figure>
 
-                            </td>
 
-                        </tr>
+                        <div><h3>
+                            ${el.price}
+                        </h3></div>
+                        <div>
+                            <Badge pill bg="info">
+                                {el.quantity}
+                            </Badge>{' '}
+                            <Button variant="danger">Remove</Button>
 
-                    ))
-                }
-                <p><strong>Total : </strong> $27.09</p>
-            </tbody>
+                        </div>
+                    </Stack>
+                ))
+            }
+
+            <h3><strong>Total : </strong> $27.09</h3>
             <Button variant='info'>Purchase</Button>
-        </Table>
+        </div>
+
     )
 }
 
